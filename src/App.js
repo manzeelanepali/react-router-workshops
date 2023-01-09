d0cad142a315ac8946bbf7cc5941a4cb78f7a542
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-09 16:44:23 +0545

    feat : we imported the usestates and initialise the notes... also changed the function notes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link
 } from "react-router-dom"
-
+ import { useState } from "react"
 
 
 const Home = () => (
   <div> <h2>TKTL notes app</h2> </div>
 )
 
-const Notes = () => (
-  <div> <h2>Notes</h2> </div>
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
 )
 
 const Users = () => (
@@ -19,6 +28,30 @@ const Users = () => (
 
 
 const App =()=>{
+
+
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+
+  const [user, setUser] = useState(null)
 
   const padding = {
     padding: 5
@@ -33,7 +66,7 @@ const App =()=>{
     </div>
 
     <Routes>
-        <Route path="/notes" element={<Notes />} />
+        <Route path="/notes" element={<Notes notes={notes}/>} />
         <Route path="/users" element={<Users />} />
         <Route path="/" element={<Home />} />
       </Routes>
