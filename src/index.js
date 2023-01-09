1e4b8f2717187d51a3d35d737889062b4127e523
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-09 16:23:39 +0545

    feat : seeing how the react router feature works

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+
+// import {
+//   BrowserRouter as Router,
+//   Routes, Route, Link
+// } from "react-router-dom"
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
