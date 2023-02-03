3c6e9d4a3bc24eb3eebe1eb373c42930aef486bb
manzeelanepali <nepalimanzeela@gmail.com>
2023-02-03 10:34:30 +0545

    feat : added table stripes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { useState } from "react";
+import { Table } from "react-bootstrap";
 
 const Home = () => (
   <div>
@@ -19,13 +20,18 @@ const Home = () => (
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
